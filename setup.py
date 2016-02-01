9a0489fbc5265a04ab10e2321a9c8c2fbc2626c5
James Muscat <jamesremuscat@gmail.com>
2016-02-01 20:18:33 +0000

    Python project skeleton.

---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+import re
+
+VERSIONFILE = "src/livetiming/_version.py"
+verstr = "unknown"
+try:
+    verstrline = open(VERSIONFILE, "rt").read()
+    VSRE = r"^__version__ = ['\"]([^'\"]*)['\"]"
+    mo = re.search(VSRE, verstrline, re.M)
+    if mo:
+        verstr = mo.group(1)
+except EnvironmentError:
+    print "unable to find version in %s" % (VERSIONFILE,)
+    raise RuntimeError("if %s exists, it is required to be well-formed" % (VERSIONFILE,))
+
+setup(
+    name='livetiming',
+    version=verstr,
+    description='Live timing aggregator for motorsport timing feeds',
+    author='James Muscat',
+    author_email='jamesremuscat@gmail.com',
+    url='https://github.com/jamesremuscat/livetiming',
+    packages=find_packages('src', exclude=["*.tests"]),
+    package_dir = {'':'src'},
+      long_description="Live timing aggregator and web service for motorsport timing feeds.",
+    install_requires=[],
+    entry_points={
+        'console_scripts': [
+            ],
+        }
+      )
