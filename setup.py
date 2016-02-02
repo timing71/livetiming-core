0a4d3fca7223b153ac0310e807224d5e847b7fa1
James Muscat <jamesremuscat@gmail.com>
2016-02-02 14:15:05 +0000

    Rename module to "directory", since that is what it will be.

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ setup(
     install_requires=["autobahn[twisted]"],
     entry_points={
         'console_scripts': [
-                'livetiming-server = livetiming.server:main'
+                'livetiming-directory = livetiming.directory:main'
             ],
         }
       )
