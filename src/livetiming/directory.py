bdd359265a8a43729a56b5c6e8584bcfe502a3dd
James Muscat <jamesremuscat@gmail.com>
2016-02-02 23:43:38 +0000

    Stop spamming the example message already.

--- src/livetiming/directory.py
+++ src/livetiming/directory.py
@@ -1,6 +1,6 @@
-from autobahn.twisted.util import sleep
 from autobahn.twisted.wamp import ApplicationSession, ApplicationRunner
 from livetiming.messaging import Channel, Message, MessageClass, Realm
+from twisted.internet import reactor
 from twisted.internet.defer import inlineCallbacks
 from twisted.internet.error import ConnectionRefusedError
 from twisted.logger import Logger
@@ -11,27 +11,27 @@ class Directory(ApplicationSession):
 
     @inlineCallbacks
     def onJoin(self, details):
-        self.log.info("session ready")
+        self.log.info("Session ready")
 
         yield self.subscribe(self.onControlMessage, Channel.CONTROL)
         self.log.debug("Subscribed to control channel")
         yield self.publish(Channel.CONTROL, Message(MessageClass.INITIALISE_DIRECTORY).serialise())
         self.log.debug("Published init message")
-        counter = 0
-        while True:
-            self.publish(u'com.myapp.oncounter', counter)
-            counter += 1
-            yield sleep(1)
 
     def onControlMessage(self, message):
         self.log.info("Received message {}".format(Message.parse(message)))
+
+    def onDisconnect(self):
+        self.log.info("Disconnected")
+        if reactor.running:
+            reactor.stop()
 
 
 def main():
     Logger().info("Starting directory service...")
     while True:
         try:
-            runner = ApplicationRunner(url=u"ws://localhost:5080/ws", realm=Realm.TIMING)
+            runner = ApplicationRunner(url=u"ws://crossbar:8080/ws", realm=Realm.TIMING)
             runner.run(Directory)
         except ConnectionRefusedError:
             pass
