cf72f3520dd504837219e866a1d69c3f81b7c589
James Muscat <jamesremuscat@gmail.com>
2016-02-02 23:52:24 +0000

    Register directory list method for RPC.

--- src/livetiming/directory.py
+++ src/livetiming/directory.py
@@ -9,6 +9,13 @@ from twisted.logger import Logger
 class Directory(ApplicationSession):
     log = Logger()
 
+    def __init__(self, config):
+        ApplicationSession.__init__(self, config)
+        self.services = {}
+
+    def listServices(self):
+        return self.services.values()
+
     @inlineCallbacks
     def onJoin(self, details):
         self.log.info("Session ready")
@@ -17,6 +24,8 @@ class Directory(ApplicationSession):
         self.log.debug("Subscribed to control channel")
         yield self.publish(Channel.CONTROL, Message(MessageClass.INITIALISE_DIRECTORY).serialise())
         self.log.debug("Published init message")
+        yield self.register(self.listServices, u"livetiming.directory.listServices")
+        self.log.debug("Registered service listing RPC")
 
     def onControlMessage(self, message):
         self.log.info("Received message {}".format(Message.parse(message)))
@@ -31,7 +40,7 @@ def main():
     Logger().info("Starting directory service...")
     while True:
         try:
-            runner = ApplicationRunner(url=u"ws://crossbar:8080/ws", realm=Realm.TIMING)
+            runner = ApplicationRunner(url=u"ws://localhost:5080/ws", realm=Realm.TIMING)
             runner.run(Directory)
         except ConnectionRefusedError:
             pass
