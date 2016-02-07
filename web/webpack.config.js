adf0ebf35bc63017b1af39d202717251f8e9d725
James Muscat <jamesremuscat@gmail.com>
2016-02-07 17:29:43 +0000

    Skeleton web project for the web UI.

---
+++ web/webpack.config.js
@@ -0,0 +1,62 @@
+const webpack = require('webpack');
+const merge = require('webpack-merge');
+const path = require('path');
+
+const TARGET = process.env.npm_lifecycle_event;
+
+const PATHS = {
+  app: path.join(__dirname, 'app'),
+  build: path.join(__dirname, 'build')
+};
+
+const common = {
+  entry: PATHS.app,
+  output: {
+    path: PATHS.build,
+    filename: 'bundle.js'
+  },
+  module: {
+   loaders: [
+      {
+        // Test expects a RegExp! Note the slashes!
+        test: /\.css$/,
+        loaders: ['style', 'css'],
+        // Include accepts either a path or an array of paths.
+        include: PATHS.app
+      }
+    ]
+  }
+};
+
+
+// Default configuration
+if(TARGET === 'start' || !TARGET) {
+  module.exports = merge(common, {
+    devtool: 'eval-source-map',
+    devServer: {
+      contentBase: PATHS.build,
+
+      // Enable history API fallback so HTML5 History API based
+      // routing works. This is a good default that will come
+      // in handy in more complicated setups.
+      historyApiFallback: true,
+      hot: true,
+      inline: true,
+      progress: true,
+
+      // Display only errors to reduce the amount of output.
+      stats: 'errors-only',
+
+      // Parse host and port from env so this is easy to customize.
+      host: process.env.HOST,
+      port: process.env.PORT
+    },
+    plugins: [
+      new webpack.HotModuleReplacementPlugin()
+    ]
+  });
+}
+
+if(TARGET === 'build') {
+  module.exports = merge(common, {});
+}
