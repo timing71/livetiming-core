53dde612066e813f71aedbfa75f78de2c65ec012
James Muscat <jamesremuscat@gmail.com>
2016-02-08 00:01:10 +0000

    Start (hackily) querying directory for a list of services.

--- web/app/components/App.jsx
+++ web/app/components/App.jsx
@@ -3,8 +3,23 @@ import autobahn from '../lib/autobahn';
 
 export default class App extends React.Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {"services": []};
+  }
+
   componentWillMount() {
     console.log("Ok, Autobahn loaded", autobahn.version);
+    const connection = new autobahn.Connection({
+      url: "ws://127.0.0.1:5080/ws",
+      realm: "timing"
+    });
+    connection.onopen = (session, details) => {
+      session.call("livetiming.directory.listServices").then((result) => {
+        this.setState({"services": result});
+      });
+    };
+    connection.open();
   }
 
   render() {
