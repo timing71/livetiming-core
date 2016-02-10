d2eef76f492d0fba93124b3198101ce629c891fe
James Muscat <jamesremuscat@gmail.com>
2016-02-10 00:56:33 +0000

    Start to make a page out of it.

--- web/app/screens/ServiceSelectionScreen.jsx
+++ web/app/screens/ServiceSelectionScreen.jsx
@@ -1,9 +1,29 @@
 import React from 'react';
 
+import { Grid, Row, Col, PageHeader } from 'react-bootstrap';
+
 import ServiceList from '../components/ServiceList';
 
 export default class ServiceSelectionScreen extends React.Component {
   render() {
-    return <ServiceList services={this.props.services} onChooseService={this.props.onChooseService} />
+    return (
+      <Grid>
+        <Row>
+          <Col md={12}>
+            <PageHeader>Live Timing Aggregator</PageHeader>
+          </Col>
+        </Row>
+        <Row>
+          <Col md={12}>
+            <ServiceList services={this.props.services} onChooseService={this.props.onChooseService} />
+          </Col>
+        </Row>
+        <Row>
+          <Col md={4} mdOffset={8}>
+            <p>Copyright &copy; James Muscat 2016. Feed data remains owner of original data source.</p>
+          </Col>
+        </Row>
+      </Grid>
+    );
   }
 }
