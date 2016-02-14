72b0e2bc15ea3024c3ab6f3e31bac0d8918c42e6
James Muscat <jamesremuscat@gmail.com>
2016-02-14 19:45:45 +0000

    Style for timing table: running vs pitted cars.

--- web/app/components/TimingTable.jsx
+++ web/app/components/TimingTable.jsx
@@ -5,15 +5,25 @@ import { Table } from 'react-bootstrap';
 
 import { format } from '../utils/formats';
 
+function findStateIndex(columnSpec) {
+  for (var i=0; i < columnSpec.length; i++) {
+    if (columnSpec[i][0] == "State") {
+      return i;
+    }
+  }
+  return -1;
+}
+
 class TimingRow extends React.Component {
   render() {
     const {position, car, columnSpec} = this.props;
     const cols = [<td key={0} className="timing-row-position">{position}</td>];
     _(columnSpec).forEach((col, index) => {
-      cols.push(<td key={index + 1}>{format(car[index], col[1])}</td>);
+      cols.push(<td key={index + 1} className={"column_" + col[0]}>{format(car[index], col[1])}</td>);
     })
+    const stateCol = findStateIndex(columnSpec)
     return (
-      <tr>
+      <tr className={"car_state_" + car[stateCol]} >
         {cols}
       </tr>
     );
