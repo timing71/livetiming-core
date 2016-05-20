83e022261293deb68b88c871bcf4b845124df026
James Muscat <james.muscat@alces-software.com>
2016-05-20 10:13:59 +0100

    Include Google Analytics tracking.

--- web/app/index.jsx
+++ web/app/index.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ga from 'react-ga';
 import { Router, browserHistory } from 'react-router';
 import routes from './routes';
 
+ga.initialize('UA-78071221-1');
+
+function logPageView() {
+  ga.pageview(window.location.pathname);
+}
+
 ReactDOM.render(
-  <Router history={browserHistory}>{routes}</Router>,
+  <Router history={browserHistory} onUpdate={logPageView}>{routes}</Router>,
   document.getElementById('app')
 );
