2a26c2718f7dbe9c2b96e082a164a9fae482e93c
James Muscat <james.muscat@alces-software.com>
2016-06-15 17:24:37 +0100

    Start to record state-by-state.

--- src/livetiming/recording.py
+++ src/livetiming/recording.py
@@ -1,6 +1,9 @@
 from datetime import datetime
 from twisted.logger import Logger
 
+import simplejson
+import zipfile
+
 
 class TimingRecorder(object):
     def __init__(self, recordFile):
@@ -10,4 +13,7 @@ class TimingRecorder(object):
         self.log = Logger()
 
     def writeState(self, state):
-        self.log.info("STUB TimingRecorder.writeState()")
+        timeDelta = (datetime.now() - self.startTime).total_seconds()
+        with zipfile.ZipFile(self.recordFile, 'a', zipfile.ZIP_DEFLATED) as z:
+            z.writestr("{:05d}.json".format(int(timeDelta)), simplejson.dumps(state))
+        self.frames += 1
