ec2a4f8226e492378b0da449520ebf1f0620a098
James Muscat <james.muscat@alces-software.com>
2016-06-24 16:50:25 +0100

    Add a RecordingProvider to feed recorded data to TimingScreen.

---
+++ web/app/containers/RecordingProvider.jsx
@@ -0,0 +1,62 @@
+import React from 'react';
+
+import TimingScreen from '../screens/TimingScreen';
+import {ServiceNotAvailable} from '../components/Modals';
+
+export default class RecordingProvider extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      time: 0,
+      recordedState: {
+        "cars": [],
+        "messages": [],
+        "session": {
+          "flagState": "none",
+          "timeElapsed": 0,
+          "timeRemain": 0
+        }
+      }
+    }
+  }
+
+  componentWillReceiveProps(newProps) {
+    const service = _(newProps.recordings).find((svc) => svc.uuid === this.props.params.recordingUUID);
+
+    if (service) {
+      this.setState({...this.state, service: service});
+      if (this.props.session) {
+        this.setTime(0, service);
+      }
+    }
+  }
+
+  setTime(time, useService) {
+    const service = useService || this.state.service;
+    if (!service) {
+      console.log("setTime called without a service");
+      return;
+    }
+    this.props.session.call(`livetiming.service.requestState.${service.uuid}`, [time]).then(
+      (result) => {
+        this.setState({
+          ...this.state,
+          time: time,
+          recordedState: result
+        });
+      },
+      (error) => {
+        console.log("Error:", error);
+      }
+    );
+  }
+
+  render() {
+    if (!this.state.service) {
+      return <ServiceNotAvailable />;
+    }
+    const {session, cars, messages} = this.state.recordedState;
+    return <TimingScreen service={this.state.service} session={session} cars={cars} messages={messages} menu={null} />
+  }
+}
--- web/app/routes.js
+++ web/app/routes.js
@@ -3,11 +3,13 @@ import { IndexRoute, Route } from 'react-router';
 
 import App from './components/App.jsx';
 import TimingServiceProvider from './containers/TimingServiceProvider';
+import RecordingProvider from './containers/RecordingProvider';
 import ServiceSelectionScreen from './screens/ServiceSelectionScreen';
 
 const routes = <Route path="/" component={App}>
   <IndexRoute component={ServiceSelectionScreen} />
   <Route path="timing/:serviceUUID" component={TimingServiceProvider} />
+  <Route path="recording/:recordingUUID" component={RecordingProvider} />
 </Route>;
 
 export default routes;
