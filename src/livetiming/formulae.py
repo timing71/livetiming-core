6ef3b3f3e92887e57feb00e0697a50c5cd959bb7
James Muscat <james.muscat@alces-software.com>
2016-06-30 14:10:28 +0100

    First pass at Formula E timing.

---
+++ src/livetiming/formulae.py
@@ -0,0 +1,112 @@
+from livetiming.service import Service as lt_service
+import urllib2
+import simplejson
+
+
+def parseTime(rawTime):
+    try:
+        parsedValue = int(rawTime)
+        if parsedValue > 3599990:
+            return ""
+        return parsedValue / 1000.0
+    except:
+        return rawTime
+
+
+class Service(lt_service):
+    def __init__(self, config):
+        lt_service.__init__(self, config)
+        self.sessionID = self.getSessionID()
+
+    def getName(self):
+        return "FIA Formula E"
+
+    def getDescription(self):
+        return "FIA Formula E"
+
+    def getColumnSpec(self):
+        return [
+            ("Num", "text"),
+            ("Driver", "text"),
+            ("Team", "text"),
+            ("Laps", "numeric"),
+            ("Gap", "delta"),
+            ("Int", "delta"),
+            ("Last", "time"),
+            ("S1", "time"),
+            ("S2", "time"),
+            ("S3", "time"),
+            ("Best", "time")
+        ]
+
+    def getPollInterval(self):
+        return 1
+
+    def getRaceState(self):
+        raw = self.getRawFeedData()
+        cars = []
+        overallBests = [99999, 99999, 99999, 99999]
+        if "data" in raw:
+            for car in sorted([car for car in raw["data"].itervalues() if "id" in car], key=lambda car: int(car["pos"])):
+                gap = parseTime(car["gap"])
+
+                bests = [
+                    parseTime(car["best_sector_1"]),
+                    parseTime(car["best_sector_2"]),
+                    parseTime(car["best_sector_3"]),
+                ]
+
+                lastLap = parseTime(car["lap_time"])
+                lastS1 = parseTime(car["sector_1"])
+                lastS2 = parseTime(car["sector_2"])
+                lastS3 = parseTime(car["sector_3"])
+                bestLap = parseTime(car["best_lap"])
+
+                overallBests = [
+                    min(bestLap, overallBests[0]),
+                    min(bests[0], overallBests[1]),
+                    min(bests[1], overallBests[2]),
+                    min(bests[2], overallBests[3])
+                ]
+
+                cars.append([
+                    car["id"],
+                    car["name"],
+                    car["team"],
+                    car["laps"],
+                    gap,
+                    gap - cars[-1][4] if cars and isinstance(cars[-1][4], float) and isinstance(gap, float) else "",
+                    [lastLap, "pb" if lastLap == bestLap else ""],
+                    [lastS1, "pb" if lastS1 == bests[0] else ""],
+                    [lastS2, "pb" if lastS2 == bests[1] else ""],
+                    [lastS3, "pb" if lastS3 == bests[2] else ""],
+                    [parseTime(car["best_lap"]), ""]
+                ])
+
+            # Annotate overall best times
+            for car in cars:
+                if car[6][0] == overallBests[0]:
+                    car[6][1] = "sb-new"
+                if car[7][0] == overallBests[1]:
+                    car[7][1] = "sb"
+                if car[8][0] == overallBests[2]:
+                    car[8][1] = "sb"
+                if car[9][0] == overallBests[3]:
+                    car[9][1] = "sb"
+                if car[10][0] == overallBests[0]:
+                    car[10][1] = "sb"
+
+        return {"cars": cars, "session": {"flagState": "green"}}
+
+    def getRawFeedData(self):
+        if self.sessionID:
+            feed_url = "http://telemetry.dc-formulae.com/api/timing/all?sessionid={}".format(self.sessionID)
+            feed = urllib2.urlopen(feed_url)
+            return simplejson.loads(feed.read())
+        return {}
+
+    def getSessionID(self):
+        sessionURL = "http://telemetry.dc-formulae.com/api/timing/sessionid?sessionid=911cfcbe-1973-4c9c-b6f3-19cbfadeb00f"
+        sessionFeed = urllib2.urlopen(sessionURL)
+        session = simplejson.loads(sessionFeed.read())
+        return session["sessionid"]
