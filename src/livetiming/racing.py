31804841d4f29e104eed5711c21a81be126021af
James Muscat <jamesremuscat@gmail.com>
2017-01-11 17:22:48 +0000

    Add a new structured way to list columns in a spec.

--- src/livetiming/racing.py
+++ src/livetiming/racing.py
@@ -16,3 +16,30 @@ class FlagStatus(Enum):
     @classmethod
     def fromString(fs, string):
         return FlagStatus[string.upper()]
+
+
+class Stat(Enum):
+    NUM = ("Num", "text")
+    STATE = ("State", "text")
+    CLASS = ("Class", "class")
+    TEAM = ("Team", "text")
+    DRIVER = ("Driver", "text")
+    CAR = ("Car", "text")
+    TYRE = ("T", "text")
+    TYRE_STINT = ("TS", "text", "Tyre stint - laps on these tyres since last stop")
+    TYRE_AGE = ("TA", "text", "Tyre age - total laps on these tyres from new")
+    LAPS = ("Laps", "numeric")
+    GAP = ("Gap", "delta", "Gap to leader")
+    INT = ("Int", "delta", "Interval to car in front")
+    LAST_LAP = ("Last", "time")
+    BEST_LAP = ("Best", "time")
+    S1 = ("S1", "time", "Sector 1 time")
+    S2 = ("S2", "time", "Sector 2 time")
+    S3 = ("S3", "time", "Sector 3 time")
+    SPEED = ("Spd", "numeric")
+    PITS = ("Pits", "numeric")
+
+    def __init__(self, title, ttype, description=None):
+        self.title = title
+        self.type = ttype
+        self.description = description
