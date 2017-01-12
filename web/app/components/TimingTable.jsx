1c5979cf21e551688f5bef24726519386baafa1d
James Muscat <jamesremuscat@gmail.com>
2017-01-12 18:09:59 +0000

    Mmm, stateless components.

--- web/app/components/TimingTable.jsx
+++ web/app/components/TimingTable.jsx
@@ -33,38 +33,35 @@ function findRaceNumberIndex(columnSpec) {
   return -1;
 }
 
-class TimingRow extends React.Component {
-  render() {
-    const {position, car, columnSpec} = this.props;
-    const cols = [<td key={0} className="timing-row-position">{position}</td>];
-    _(columnSpec).forEach((col, index) => {
-      const valTuple = car[index];
-      let value, flags;
-      if (typeof(valTuple) == "object") {
-        value = valTuple[0];
-        flags = valTuple[1];
-      }
-      else {
-        value = valTuple;
-        flags = "";
-      }
-      cols.push(<td key={index + 1} className={`column_${col[0]} ${flags}`}>{format(value, col[1])}</td>);
-    });
+const TimingRow = ({position, car, columnSpec}) => {
+  const cols = [<td key={0} className="timing-row-position">{position}</td>];
+  _(columnSpec).forEach((col, index) => {
+    const valTuple = car[index];
+    let value, flags;
+    if (typeof(valTuple) == "object") {
+      value = valTuple[0];
+      flags = valTuple[1];
+    }
+    else {
+      value = valTuple;
+      flags = "";
+    }
+    cols.push(<td key={index + 1} className={`column_${col[0]} ${flags}`}>{format(value, col[1])}</td>);
+  });
 
-    const hasSetSB = _(car).find((c) => (typeof(c) == "object" && c[1] == "sb-new"));
-    const stateCol = findStateIndex(columnSpec);
-    const classCol = findClassIndex(columnSpec);
-    const carClass = classNameFromCategory(car[classCol]);
-    const carNumIdx = findRaceNumberIndex(columnSpec);
-    const carNum = carNumIdx > -1 ? `car_${car[carNumIdx]}` : null;
-    const carState = car[stateCol].replace(/[\/ ]/g, "");
-    return (
-      <tr id={carNum} className={`car_state_${carState} car_class_${carClass} ${hasSetSB? "sb-new" : ""}`} >
-        {cols}
-      </tr>
-    );
-  }
-}
+  const hasSetSB = _(car).find((c) => (typeof(c) == "object" && c[1] == "sb-new"));
+  const stateCol = findStateIndex(columnSpec);
+  const classCol = findClassIndex(columnSpec);
+  const carClass = classNameFromCategory(car[classCol]);
+  const carNumIdx = findRaceNumberIndex(columnSpec);
+  const carNum = carNumIdx > -1 ? `car_${car[carNumIdx]}` : null;
+  const carState = car[stateCol].replace(/[\/ ]/g, "");
+  return (
+    <tr id={carNum} className={`car_state_${carState} car_class_${carClass} ${hasSetSB? "sb-new" : ""}`} >
+      {cols}
+    </tr>
+  );
+};
 
 const TimingTableHeader = ({spec}) => {
   if (spec.length == 3) {
@@ -81,24 +78,24 @@ const TimingTableHeader = ({spec}) => {
   }
 };
 
-export default class TimingTable extends React.Component {
-  render() {
-    const carRows = [];
-    _(this.props.cars).forEach((car, position) => {
-      carRows.push(<TimingRow car={car} key={position} position={position + 1} columnSpec={this.props.columnSpec} />);
-    });
-    return (
-      <Table striped className="timing-table table-condensed">
-        <thead>
-          <tr className="timing-table-header">
-            <td>Pos</td>
-            {this.props.columnSpec.map((spec, idx) => <TimingTableHeader key={idx} spec={spec} />)}
-          </tr>
-        </thead>
-        <tbody>
-          {carRows}
-        </tbody>
-      </Table>
-    );
-  }
-}
+const TimingTable = ({cars, columnSpec}) => {
+  const carRows = [];
+  _(cars).forEach((car, position) => {
+    carRows.push(<TimingRow car={car} key={position} position={position + 1} columnSpec={columnSpec} />);
+  });
+  return (
+    <Table striped className="timing-table table-condensed">
+      <thead>
+        <tr className="timing-table-header">
+          <td>Pos</td>
+          {columnSpec.map((spec, idx) => <TimingTableHeader key={idx} spec={spec} />)}
+        </tr>
+      </thead>
+      <tbody>
+        {carRows}
+      </tbody>
+    </Table>
+  );
+};
+
+export default TimingTable;
