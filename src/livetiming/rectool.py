5e000626d242712ed9229641bf4fe86d9950c68b
James Muscat <jamesremuscat@gmail.com>
2017-01-16 17:00:01 +0000

    Change recording format and tool support.
    Recordings now save frames with a timestamp rather than a delta. This
    will make trimming recordings easier!
    
    We now store a version number in the recording manifest. If it's not
    there we assume a pre-v1 file and throw an error (unless compatibility
    is forced - which makes no guarantees about actually working).

--- src/livetiming/rectool.py
+++ src/livetiming/rectool.py
@@ -1,59 +1,11 @@
 import argparse
-import datetime
 import os
-import re
+import shutil
 import simplejson
 import tempfile
 import zipfile
 
-
-# http://stackoverflow.com/a/25739108/11643
-def updateZip(zipname, filename, data):
-    # generate a temp file
-    tmpfd, tmpname = tempfile.mkstemp(dir=os.path.dirname(zipname))
-    os.close(tmpfd)
-
-    # create a temp copy of the archive without filename
-    with zipfile.ZipFile(zipname, 'r') as zin:
-        with zipfile.ZipFile(tmpname, 'w') as zout:
-            zout.comment = zin.comment  # preserve the comment
-            seen_filenames = []
-            for item in zin.infolist():
-                if item.filename != filename and item.filename not in seen_filenames:
-                    zout.writestr(item, zin.read(item.filename))
-                    seen_filenames.append(item.filename)
-
-    # replace with the temp archive
-    os.remove(zipname)
-    os.rename(tmpname, zipname)
-
-    # now add filename with its new data
-    with zipfile.ZipFile(zipname, mode='a', compression=zipfile.ZIP_DEFLATED) as zf:
-        zf.writestr(filename, data)
-
-
-class RecordingFile(object):
-    def __init__(self, filename):
-        self.filename = filename
-        self.iframes = []
-        self.keyframes = []
-        with zipfile.ZipFile(filename, 'r', zipfile.ZIP_DEFLATED) as z:
-            maxFrame = 0
-            for frame in z.namelist():
-                m = re.match("([0-9]{5})(i?)", frame)
-                if m:
-                    val = int(m.group(1))
-                    if m.group(2):  # it's an iframe
-                        self.iframes.append(val)
-                    else:
-                        self.keyframes.append(val)
-                    maxFrame = max(val, maxFrame)
-            self.duration = maxFrame
-            self.manifest = simplejson.load(z.open("manifest.json", 'r'))
-        self.frames = len(self.iframes) + len(self.keyframes)
-
-    def save_manifest(self):
-        updateZip(self.filename, "manifest.json", simplejson.dumps(self.manifest))
+from livetiming.recording import RecordingFile
 
 
 def describe(args, extras):
@@ -71,12 +23,36 @@ def inspect(args, extras):
     print "##########"
     print "Service: {} ({})".format(f.manifest['name'], f.manifest['uuid'])
     print "{} frames ({}k/{}i), {} duration".format(f.frames, len(f.keyframes), len(f.iframes), f.duration)
-    print "Start time: {}".format(datetime.datetime.fromtimestamp(f.manifest['startTime']))
+    print "Start time: {}".format(f.startTime)
+
+
+def convert(args, extras):
+    orig = RecordingFile(args.recfile, force_compat=True)
+    startTime = orig.manifest['startTime']
+
+    tempdir = tempfile.mkdtemp(suffix=".rectool-convert")
+
+    with zipfile.ZipFile(args.recfile, 'r', zipfile.ZIP_DEFLATED) as z:
+        z.extractall(tempdir)
+
+    with zipfile.ZipFile("conv_{}".format(args.recfile), 'w', zipfile.ZIP_DEFLATED) as z:
+        for frame in orig.keyframes:
+            print "{} => {}".format(frame, int(startTime + frame))
+            z.write(os.path.join(tempdir, "{:05d}.json".format(frame)), "{:011d}.json".format(int(startTime + frame)))
+        for frame in orig.iframes:
+            print "{}i => {}i".format(frame, int(startTime + frame))
+            z.write(os.path.join(tempdir, "{:05d}i.json".format(frame)), "{:011d}i.json".format(int(startTime + frame)))
+        orig.manifest['version'] = 1
+        del orig.manifest['startTime']
+        z.writestr("manifest.json", simplejson.dumps(orig.manifest))
+
+    shutil.rmtree(tempdir)
 
 
 ACTIONS = {
     'inspect': inspect,
-    'describe': describe
+    'describe': describe,
+    'convert': convert
 }
 
 
