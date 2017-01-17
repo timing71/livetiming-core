78f81290c1417fe31b834c177427768486f49e06
James Muscat <jamesremuscat@gmail.com>
2017-01-17 18:05:32 +0000

    Allow specifying a default value if stat not found.
    This essentially means that all the services can now have virtually
    identical getMessageGenerators methods, the only exceptions being ones
    that work in service-specific ways e.g. race control messages that need
    more state than the defaults.

--- src/livetiming/messages.py
+++ src/livetiming/messages.py
@@ -1,5 +1,6 @@
-from racing import FlagStatus
 import time
+
+from racing import FlagStatus
 from livetiming.racing import Stat
 
 
@@ -20,20 +21,14 @@ class TimingMessage(object):
 
 
 class PerCarMessage(TimingMessage):
-    def __init__(self, columnSpec=None, clazz=None):
+    def __init__(self, columnSpec=None):
         self.columnSpec = columnSpec
-        self.clazz = clazz
 
-    def getValue(self, car, stat):
+    def getValue(self, car, stat, default=None):
         if self.columnSpec and stat in self.columnSpec:
             idx = self.columnSpec.index(stat)
             return car[idx]
-        return None
-
-    def getClass(self, car):
-        if self.clazz:
-            return self.clazz
-        return self.getValue(car, Stat.CLASS)
+        return default
 
     def process(self, oldState, newState):
         messages = []
@@ -87,16 +82,17 @@ class CarPitMessage(PerCarMessage):
 
         carNum = self.getValue(newCar, Stat.NUM)
         driver = self.getValue(newCar, Stat.DRIVER)
+        clazz = self.getValue(newCar, Stat.CLASS, "Pits")
 
         if oldStatus != newStatus:
             if newStatus == "OUT" or (newStatus == "RUN" and oldStatus == "PIT"):
-                return [self.getClass(newCar), u"#{} ({}) has left the pits".format(carNum, driver), "out"]
+                return [clazz, u"#{} ({}) has left the pits".format(carNum, driver), "out"]
             elif newStatus == "PIT":
-                return [self.getClass(newCar), u"#{} ({}) has entered the pits".format(carNum, driver), "pit"]
+                return [clazz, u"#{} ({}) has entered the pits".format(carNum, driver), "pit"]
             elif newStatus == "FUEL":
-                return [self.getClass(newCar), u"#{} ({}) has entered the fuelling area".format(carNum, driver), "pit"]
+                return [clazz, u"#{} ({}) has entered the fuelling area".format(carNum, driver), "pit"]
             elif newStatus == "RET":
-                return [self.getClass(newCar), u"#{} ({}) has retired".format(carNum, driver), ""]
+                return [clazz, u"#{} ({}) has retired".format(carNum, driver), ""]
 
 
 # Emits a message if the driver of a car changes.
@@ -107,7 +103,7 @@ class DriverChangeMessage(PerCarMessage):
         newDriver = self.getValue(newCar, Stat.DRIVER)
         carNum = self.getValue(newCar, Stat.NUM)
         if oldDriver != newDriver and oldDriver != "":
-            return [self.getClass(newCar), u"#{} Driver change ({} to {})".format(carNum, oldDriver, newDriver)]
+            return [self.getValue(newCar, Stat.CLASS, "Pits"), u"#{} Driver change ({} to {})".format(carNum, oldDriver, newDriver)]
 
 
 # Emits a message if a car sets a personal or overall best.
@@ -116,6 +112,7 @@ class FastLapMessage(PerCarMessage):
     def _consider(self, oldCar, newCar):
         carNum = self.getValue(newCar, Stat.NUM)
         driver = self.getValue(newCar, Stat.DRIVER)
+        clazz = self.getValue(newCar, Stat.CLASS, "Timing")
         oldTime = self.getValue(oldCar, Stat.LAST_LAP)
         newTime = self.getValue(newCar, Stat.LAST_LAP)
         oldFlags = oldTime[1]
@@ -123,6 +120,6 @@ class FastLapMessage(PerCarMessage):
 
         if newTime[0] > 0 and (oldFlags != newFlags or oldTime[0] != newTime[0]):
             if newFlags == "pb" and oldFlags == "":
-                return [self.getClass(newCar), u"#{} ({}) set a new personal best: {}".format(carNum, driver, formatTime(newTime[0])), "pb"]
+                return [clazz, u"#{} ({}) set a new personal best: {}".format(carNum, driver, formatTime(newTime[0])), "pb"]
             elif newFlags == "sb-new":
-                return [self.getClass(newCar), u"#{} ({}) set a new overall best: {}".format(carNum, driver, formatTime(newTime[0])), "sb"]
+                return [clazz, u"#{} ({}) set a new overall best: {}".format(carNum, driver, formatTime(newTime[0])), "sb"]
