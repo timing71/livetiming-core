1fd50882553f9162fd47d699c9bbf0d7bdd1c2d5
James Muscat <jamesremuscat@gmail.com>
2017-01-30 12:26:34 +0000

    Tidy up and document Service class.

--- src/livetiming/service.py
+++ src/livetiming/service.py
@@ -2,15 +2,15 @@ from autobahn.twisted.wamp import ApplicationSession, ApplicationRunner
 from livetiming.messages import FlagChangeMessage, CarPitMessage,\
     DriverChangeMessage, FastLapMessage
 from livetiming.network import Channel, Message, MessageClass, Realm, RPC
-from livetiming.racing import FlagStatus, Stat
+from livetiming.racing import Stat
 from livetiming.recording import TimingRecorder
 from os import environ, path
-from random import randint
 from twisted.internet import reactor
 from twisted.internet.defer import inlineCallbacks
 from twisted.internet.task import LoopingCall
 from twisted.logger import Logger
 from uuid import uuid4
+
 import argparse
 import copy
 import simplejson
@@ -25,14 +25,112 @@ class Service(ApplicationSession):
         ApplicationSession.__init__(self, config)
         self.args = config.extra
         self.uuid = path.splitext(self.args["initial_state"])[0] if self.args["initial_state"] is not None else uuid4().hex
-        self.state = self.getInitialState()
+        self.state = self._getInitialState()
         if self.args["recording_file"] is not None:
             self.recorder = TimingRecorder(self.args["recording_file"])
-            self.recorder.writeManifest(self.createServiceRegistration())
+            self.recorder.writeManifest(self._createServiceRegistration())
         else:
             self.recorder = None
 
-    def getInitialState(self):
+    ###################################################
+    # These methods MUST be implemented by subclasses #
+    ###################################################
+
+    def getName(self):
+        '''
+        Must be implemented by subclasses to return the string used as a
+        name for this service.
+        If the value returned by this function is not constant then the
+        service will probably want to call self.publishManifest() when the
+        value changes, or the change will not propagate to clients.
+        '''
+        raise NotImplementedError
+
+    def getDefaultDescription(self):
+        '''
+        Must be implemented by subclasses to return the string used as a
+        description, unless one has been provided at runtime with -d.
+        If the value returned by this function is not constant then the
+        service will probably want to call self.publishManifest() when the
+        value changes, or the change will not propagate to clients.
+        '''
+        raise NotImplementedError
+
+    def getColumnSpec(self):
+        '''
+        Must be implemented by subclasses to return a list of Stat objects
+        representing the list of available columns to display.
+        If the value returned by this function is not constant then the
+        service will probably want to call self.publishManifest() when the
+        value changes, or the change will not propagate to clients.
+        '''
+        raise NotImplementedError
+
+    def getRaceState(self):
+        '''
+        Must be implemented by subclasses to return an dict containing two keys:
+        {
+          'cars': [...list of car stat lists...],
+          'state': { ... dict of state values ... }
+        }
+
+        Each entry in 'cars' should be a list of values matching the column spec.
+        All times should be in decimal seconds - this includes sector and lap times.
+
+        Keys in 'state' can include:
+         - flagState (livetiming.racing.FlagState.<flag>.name.lower())
+         - timeElapsed (in seconds)
+         - timeRemain (in seconds)
+         - lapsRemain (integer)
+         - trackData (list of formatted strings to display as track data)
+
+        No filtering is performed; all values herein will be sent to clients.
+        This means they need to be serializable e.g. plain Python types, not
+        objects.
+        '''
+        raise NotImplementedError
+
+    #################################################
+    # These methods MAY be overridden by subclasses #
+    #################################################
+
+    def getTrackDataSpec(self):
+        '''
+        May be overridden by subclasses to provide a list of strings that are
+        the keys of the key/value pairs of track data to display.
+        Defaults to an empty list.
+        '''
+        return []
+
+    def getPollInterval(self):
+        '''
+        May be overridden by clients to specify the interval, in seconds, at
+        which self.getRaceState() will be called and the latest state published
+        to clients.
+
+        TODO: Push-based (websockety) services would be better served
+        publishing updates directly and immediately rather than themselves
+        being polled...
+        '''
+        return 10
+
+    def getExtraMessageGenerators(self):
+        '''
+        May be overridden by subclasses to provide a list of additional message
+        generators (subclasses of livetiming.messages.TimingMessage) to be used
+        when generating messages for this service.
+
+        The default set of generators cannot be overridden; those generators
+        are written to be safe and silent in the event that their required data
+        is unavailable.
+        '''
+        return []
+
+    ######################################################
+    # These methods MUST NOT be overridden by subclasses #
+    ######################################################
+
+    def _getInitialState(self):
         if self.args["initial_state"] is not None:
             try:
                 stateFile = open(self.args["initial_state"], 'r')
@@ -50,7 +148,7 @@ class Service(ApplicationSession):
             "cars": []
         }
 
-    def saveState(self):
+    def _saveState(self):
         self.log.debug("Saving state of {}".format(self.uuid))
         try:
             stateFile = open("{}.json".format(self.uuid), 'w')
@@ -62,78 +160,39 @@ class Service(ApplicationSession):
         if self.recorder:
             self.recorder.writeState(self.state)
 
-    def createServiceRegistration(self):
+    def _createServiceRegistration(self):
         colspec = map(lambda s: s.value if isinstance(s, Stat) else s, self.getColumnSpec())
         return {
             "uuid": self.uuid,
             "name": self.getName(),
-            "description": self.getDescription(),
+            "description": self._getDescription(),
             "colSpec": colspec,
             "trackDataSpec": self.getTrackDataSpec(),
             "pollInterval": self.getPollInterval()
         }
 
-    def getName(self):
-        return "Generic Service"
-
-    def getDescription(self):
+    def _getDescription(self):
         if self.args['description'] is not None:
             return self.args['description']
         return self.getDefaultDescription()
 
-    def getDefaultDescription(self):
-        return "A generic service that has no purpose other than as a base class"
-
-    def getColumnSpec(self):
-        return [
-            Stat.NUM,
-            Stat.DRIVER,
-            Stat.LAPS,
-            Stat.GAP,
-            Stat.INT,
-            Stat.LAST_LAP,
-            Stat.PITS
-        ]
-
-    def getTrackDataSpec(self):
-        return []
-
-    def getPollInterval(self):
-        return 10
-
-    def isAlive(self):
+    def _isAlive(self):
         return True
 
     def _updateRaceState(self):
         try:
             newState = self.getRaceState()
-            self.state["messages"] = (self.createMessages(self.state, newState) + self.state["messages"])[0:100]
+            self.state["messages"] = (self._createMessages(self.state, newState) + self.state["messages"])[0:100]
             self.state["cars"] = copy.deepcopy(newState["cars"])
             self.state["session"] = copy.deepcopy(newState["session"])
-            self.saveState()
+            self._saveState()
         except Exception as e:
             self.log.error(e)
 
     def _updateAndPublishRaceState(self):
         self.log.info("Publishing timing data for {}".format(self.uuid))
         self._updateRaceState()
-        self.publish(unicode(self.uuid), Message(MessageClass.SERVICE_DATA, self.getTimingMessage()).serialise())
-
-    def getRaceState(self):
-        time1 = randint(90000, 95000) / 1000.0
-        time2 = randint(90000, 95000) / 1000.0
-        flag = FlagStatus(randint(0, 6)).name.lower()
-        return {
-            "cars": [
-                ["7", "DriverName", 7, 0, 0, time1, 1],
-                ["8", "Driver Two", 7, 0.123, 0.123, time2, 1]
-            ],
-            "session": {
-                "flagState": flag,
-                "timeElapsed": 0,
-                "timeRemain": 0
-            }
-        }
+        self.publish(unicode(self.uuid), self._requestCurrentState())
 
     def _getMessageGenerators(self):
         return [
@@ -143,30 +202,24 @@ class Service(ApplicationSession):
             FastLapMessage(self.getColumnSpec()),
         ]
 
-    def getExtraMessageGenerators(self):
-        return []
-
-    def createMessages(self, oldState, newState):
+    def _createMessages(self, oldState, newState):
         # Messages are of the form [time, category, text, messageType]
         messages = []
         for mg in self._getMessageGenerators() + self.getExtraMessageGenerators():
             messages += mg.process(oldState, newState)
         return messages
 
-    def getTimingMessage(self):
-        return self.state
-
-    def requestCurrentState(self):
-        return Message(MessageClass.SERVICE_DATA, self.getTimingMessage()).serialise()
+    def _requestCurrentState(self):
+        return Message(MessageClass.SERVICE_DATA, self.state).serialise()
 
     def publishManifest(self):
-        self.publish(Channel.CONTROL, Message(MessageClass.SERVICE_REGISTRATION, self.createServiceRegistration()).serialise())
+        self.publish(Channel.CONTROL, Message(MessageClass.SERVICE_REGISTRATION, self._createServiceRegistration()).serialise())
 
     @inlineCallbacks
     def onJoin(self, details):
         self.log.info("Session ready for service {}".format(self.uuid))
-        yield self.register(self.isAlive, RPC.LIVENESS_CHECK.format(self.uuid))
-        yield self.register(self.requestCurrentState, RPC.REQUEST_STATE.format(self.uuid))
+        yield self.register(self._isAlive, RPC.LIVENESS_CHECK.format(self.uuid))
+        yield self.register(self._requestCurrentState, RPC.REQUEST_STATE.format(self.uuid))
         yield self.subscribe(self.onControlMessage, Channel.CONTROL)
         self.log.info("Subscribed to control channel")
         yield self.publishManifest()
@@ -182,7 +235,7 @@ class Service(ApplicationSession):
             yield self.publishManifest()
 
     def onDisconnect(self):
-        self.log.info("Disconnected")
+        self.log.info("Disconnected from live timing service")
         if reactor.running:
             reactor.stop()
 
