8cdba11e735988dbeecbcf4406fdd470e567fbdb
James Muscat <jamesremuscat@gmail.com>
2017-01-30 17:57:58 +0000

    Make FlagStatus ordered - more severe flags higher.

--- src/livetiming/racing.py
+++ src/livetiming/racing.py
@@ -1,17 +1,17 @@
-from enum import Enum
+from enum import Enum, IntEnum
 
 
-class FlagStatus(Enum):
-    GREEN = 0
-    YELLOW = 1
-    FCY = 2
-    CODE_60 = 3
-    SC = 4
-    RED = 5
-    CHEQUERED = 6
-    WHITE = 7
-    VSC = 8
-    NONE = 999
+class FlagStatus(IntEnum):
+    NONE = 0
+    GREEN = 1
+    WHITE = 2
+    CHEQUERED = 3
+    YELLOW = 4
+    FCY = 5
+    CODE_60 = 6
+    VSC = 7
+    SC = 7
+    RED = 9
 
     @classmethod
     def fromString(fs, string):
