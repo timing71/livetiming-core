1004504440becc8b8b154e1ca130d3c21ab3ab10
James Muscat <jamesremuscat@gmail.com>
2017-02-20 12:42:15 +0000

    Replace old Formula E timing service with Al Kamel-based one.

--- src/livetiming/fe.py
+++
@@ -1,9 +0,0 @@
-from alkamel import Service as AlkamelService
-
-
-class Service(AlkamelService):
-    def __init__(self, config):
-        super(Service, self).__init__(config, feed='92200890-f282-11e3-ac10-0800200c9a66')
-
-    def getName(self):
-        return "Formula E"
