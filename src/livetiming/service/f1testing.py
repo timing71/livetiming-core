62a3da969c835f1e249713c9e669f509fe76996d
James Muscat <jamesremuscat@gmail.com>
2017-03-02 15:50:48 +0000

    Dynamically find most relevant F1 testing data.

--- src/livetiming/service/f1testing.py
+++ src/livetiming/service/f1testing.py
@@ -1,15 +1,30 @@
 from livetiming.service import Service as lt_service, Fetcher
 import simplejson
 from livetiming.racing import Stat
+from datetime import date
+
+
+def find_todays_data(all_data):
+    today = date.today().isoformat()
+    for key, tests in all_data.iteritems():
+        for day, test in tests.iteritems():
+            if "date" in test and test["date"] == today:
+                test["description"] = "{} - {}".format(key, day)
+                return test
+    return {}
 
 
 class Service(lt_service):
     def __init__(self, config):
         super(Service, self).__init__(config)
-        self.data = None
+        self.data = {}
 
         def setData(new_data):
-            self.data = simplejson.loads(new_data[28:-2])
+            all_data = simplejson.loads(new_data[28:-2])
+            old_description = self.data.get("description", None)
+            self.data = find_todays_data(all_data)
+            if self.data.get("description", None) != old_description:
+                self.publishManifest()
 
         fetcher = Fetcher("http://www.softpauer.com/f1/2017/testsessions/TestResults.js", setData, 30)
         fetcher.start()
@@ -18,7 +33,9 @@ class Service(lt_service):
         return "F1 Testing"
 
     def getDefaultDescription(self):
-        return "Barcelona day 4"
+        if "description" in self.data:
+            return self.data["description"]
+        return ""
 
     def getColumnSpec(self):
         return [
@@ -35,33 +52,30 @@ class Service(lt_service):
 
     def getRaceState(self):
         cars = []
+        stats = self.data
 
-        if "F1 Team Testing - T01 - Barcelona" in self.data:
-            barca = self.data["F1 Team Testing - T01 - Barcelona"]
-            if "Thursday" in barca:
-                stats = barca["Thursday"]
+        drivers = {}
+        for driver in stats["testEntry"]:
+            drivers[driver["racingNumber"]] = driver
 
-                drivers = {}
-                for driver in stats["testEntry"]:
-                    drivers[driver["racingNumber"]] = driver
+        for car in stats["testClassification"]:
+            driver = drivers[car["racingNumber"]]
 
-                for car in stats["testClassification"]:
-                    driver = drivers[car["racingNumber"]]
+            cars.append([
+                car["racingNumber"],
+                "PIT" if car["inPits"] else "RUN",
+                driver["driverFullName"],
+                driver["teamName"],
+                car["lapNumber"],
+                car["lapTimeL"] / 1000.0,
+                car["gap"],
+                car["difference"],
+                car["pitStops"]
+            ])
 
-                    cars.append([
-                        car["racingNumber"],
-                        "PIT" if car["inPits"] else "RUN",
-                        driver["driverFullName"],
-                        driver["teamName"],
-                        car["lapNumber"],
-                        car["lapTimeL"] / 1000.0,
-                        car["gap"],
-                        car["difference"],
-                        car["pitStops"]
-                    ])
         return {
             "session": {
-                "flagState": "green" if stats["isSessionLive"] else "red",
+                "flagState": "green" if stats["isSessionLive"] else "none",
                 "timeElapsed": 0,
                 "timeRemain": -1
             },
