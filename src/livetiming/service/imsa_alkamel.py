7214c3b6dd68696b5778bf18f85e280b76142614
James Muscat <jamesremuscat@gmail.com>
2017-03-28 16:43:57 +0100

    No longer any need to overwrite this.

--- src/livetiming/service/imsa_alkamel.py
+++ src/livetiming/service/imsa_alkamel.py
@@ -4,6 +4,3 @@ from livetiming.service.alkamel import Service as alkamel_service
 class Service(alkamel_service):
     def __init__(self, config):
         super(Service, self).__init__(config, feed="910a7e3e-x15e-93a1-1007-r8c7xa149609")
-
-    def getName(self):
-        return "IMSA"
