a3ee95857162baddf18b4623ea7e35334ae447b7
James Muscat <jamesremuscat@gmail.com>
2017-03-30 18:23:27 +0100

    Include cwd in search for env file.

--- src/livetiming/__init__.py
+++ src/livetiming/__init__.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv, find_dotenv
 
 def load_env():
     try:
-        maybe_dotenv = find_dotenv("livetiming.env", raise_error_if_not_found=True)
+        maybe_dotenv = find_dotenv("livetiming.env", raise_error_if_not_found=True, usecwd=True)
         load_dotenv(maybe_dotenv)
     except IOError:
         pass
