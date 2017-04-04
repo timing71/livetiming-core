8a95b711330f6e0f5a8117676a9a7266e6fe3ed6
James Muscat <jamesremuscat@gmail.com>
2017-04-04 12:55:09 +0100

    Basic implementation of lap chart analysis.

---
+++ src/livetiming/analysis/lapchart.py
@@ -0,0 +1,21 @@
+from livetiming.analysis import Analysis
+
+
+def _lap_chart_add_entry(chart, pos, lap, race_num):
+    if pos not in chart:
+        chart[pos] = {}
+    chart[pos][lap] = race_num
+
+
+class LapChart(Analysis):
+    def getName(self):
+        return "Lap chart"
+
+    def getData(self):
+        lap_chart = {}
+
+        for car in self.data_centre.cars:
+            for lap in car.laps:
+                _lap_chart_add_entry(lap_chart, lap.position, lap.lap_num, car.race_num)
+
+        return lap_chart
