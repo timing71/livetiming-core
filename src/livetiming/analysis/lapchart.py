3679bf9477e867b562afa460fa5549b3f8349c4c
James Muscat <jamesremuscat@gmail.com>
2017-04-04 18:09:20 +0100

    Show pit stops with a red box.

--- src/livetiming/analysis/lapchart.py
+++ src/livetiming/analysis/lapchart.py
@@ -1,13 +1,13 @@
 from livetiming.analysis import Analysis
 
 
-def _lap_chart_add_entry(chart, race_num, lap):
+def _lap_chart_add_entry(chart, race_num, lap, is_in_lap):
     pos = lap.position
     lap_num = lap.lap_num
     if pos not in chart:
         chart[pos] = {}
     if lap_num not in chart[pos]:
-        chart[pos][lap_num] = [race_num] + lap.for_json()
+        chart[pos][lap_num] = [race_num] + lap.for_json() + [is_in_lap]
 
 
 class LapChart(Analysis):
@@ -18,7 +18,8 @@ class LapChart(Analysis):
         lap_chart = {}
 
         for car in self.data_centre.cars:
+            in_laps = map(lambda s: s.end_lap, car.stints)
             for lap in car.laps:
-                _lap_chart_add_entry(lap_chart, car.race_num, lap)
+                _lap_chart_add_entry(lap_chart, car.race_num, lap, lap.lap_num in in_laps)
 
         return lap_chart
