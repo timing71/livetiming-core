63ee1c4e090f570a9d49c70b44ea0eebf3ab70d9
James Muscat <jamesremuscat@gmail.com>
2017-04-06 12:38:46 +0100

    Highlight 24H Series 'joker' laps.

--- src/livetiming/service/timeservice_nl.py
+++ src/livetiming/service/timeservice_nl.py
@@ -59,7 +59,9 @@ def mapTimeFlags(flag):
         return 'pb'
     if flag == "16736511":
         return 'sb'
-    if flag == "6579455" or flag == "4210943" or flag == "255":
+    if flag == "6579455":
+        return "joker"
+    if flag == "4210943" or flag == "255":
         # not sure that 'slow' is accurate but TSNL displays these in red
         return 'slow'
     return ""
