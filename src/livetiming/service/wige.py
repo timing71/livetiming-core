4520c177e6e44ea22fe1bfc70cef3e81b84a4712
James Muscat <jamesremuscat@gmail.com>
2017-04-24 12:29:09 +0100

    Give WIGE a name of their own.

--- src/livetiming/service/wige.py
+++ src/livetiming/service/wige.py
@@ -3,5 +3,8 @@ from livetiming.service.timeservice_nl import Service as tsnl_service
 
 class Service(tsnl_service):
 
+    def getName(self):
+        return "WIGE Timing"
+
     def getHost(self):
         return "livetiming.tracktime.info"
