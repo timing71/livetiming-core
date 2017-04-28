f1e5f01bdb1b73f2d9b5e1ca43e24fb7dadeba32
James Muscat <jamesremuscat@gmail.com>
2017-04-28 15:17:39 +0100

    Give PWC it's own title.

--- src/livetiming/service/pwc.py
+++ src/livetiming/service/pwc.py
@@ -4,6 +4,9 @@ from livetiming.service.tsl import Service as tsl_service
 
 
 class Service(tsl_service):
+    def getName(self):
+        return "Pirelli World Challenge"
+
     def getHost(self):
         return "lt-us.tsl-timing.com"
 
