e3b712df228433db8754779583a20793b28f64d3
James Muscat <jamesremuscat@gmail.com>
2017-10-02 17:38:57 +0100

    Always use `--caution` with `imsa_alkamel` service.

--- src/livetiming/service/imsa_alkamel.py
+++ src/livetiming/service/imsa_alkamel.py
@@ -3,4 +3,5 @@ from livetiming.service.alkamel import Service as alkamel_service
 
 class Service(alkamel_service):
     def __init__(self, args, extra_args):
+        extra_args.append('--caution')
         super(Service, self).__init__(args, extra_args, feed="imsa")
