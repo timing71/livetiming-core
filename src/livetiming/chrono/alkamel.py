a34eb5276f8250980bb46c682814d54b98b03d6e
James Muscat <jamesremuscat@gmail.com>
2017-12-20 16:10:37 +0000

    Fully parse times, and create some messages.

--- src/livetiming/chrono/alkamel.py
+++ src/livetiming/chrono/alkamel.py
@@ -1,8 +1,10 @@
 from datetime import datetime
 from livetiming.chrono import LaptimeEvent, SectorEvent
+from livetiming.messages import FastLapMessage
 from livetiming.racing import Stat
 from livetiming.service.wec import parseTime
 
+import calendar
 import csv
 
 
@@ -47,21 +49,28 @@ def _parse_clock_time(clock):
 
 def generate_parser_args(parser):
     parser.add_argument('--chronological-analysis', '-c', help='Chronological analysis CSV file', required=True)
+    parser.add_argument('--start-date', '-s', help='Date at start of session', required=True)
 
 
 def create_events(args):
     events = []
+
+    start_date = datetime.strptime(args.start_date, '%Y-%m-%d')
+
     with open(args.chronological_analysis, 'rb') as csvfile:
         reader = csv.DictReader(csvfile, delimiter=';')
         for row in reader:
             race_num = row['\xef\xbb\xbfNUMBER']
             clock_time = _parse_clock_time(row[' HOUR'])
 
+            ts = start_date.replace(hour=clock_time.hour, minute=clock_time.minute, second=clock_time.second)
+            datestamp = calendar.timegm(ts.timetuple())
+
             events.append(
-                (clock_time, SectorEvent(_COLSPEC, race_num, 3, parseTime(row[' S3']), _parseFlags(row[' S3_IMPROVEMENT'])))
+                (datestamp, SectorEvent(_COLSPEC, race_num, 3, parseTime(row[' S3']), _parseFlags(row[' S3_IMPROVEMENT'])))
             )
             events.append(
-                (clock_time, LaptimeEvent(_COLSPEC, race_num, parseTime(row[' LAP_TIME']), _parseFlags(row[' LAP_IMPROVEMENT'])))
+                (datestamp, LaptimeEvent(_COLSPEC, race_num, parseTime(row[' LAP_TIME']), _parseFlags(row[' LAP_IMPROVEMENT'])))
             )
     return events
 
@@ -76,9 +85,9 @@ def create_initial_state(args):
                 state[race_num] = [
                     race_num,
                     'RUN',
-                    row['CLASS'],
-                    row['DRIVER_NAME'],
-                    row['TEAM'],
+                    row['CLASS'].decode('utf-8'),
+                    row['DRIVER_NAME'].decode('utf-8'),
+                    row['TEAM'].decode('utf-8'),
                     0,
                     None,
                     None,
@@ -94,3 +103,13 @@ def create_initial_state(args):
                 ]
 
     return state
+
+
+def sort_cars(args, cars):
+    return sorted(cars, key=lambda c: c[15])
+
+
+def message_generators():
+    return [
+        FastLapMessage(_COLSPEC)
+    ]
