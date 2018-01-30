74066a722b843626124fa0883e737dcceeb38f11
James Muscat <jamesremuscat@gmail.com>
2018-01-30 17:45:43 +0000

    Pull out watchdog functionality so it can be reused.

--- src/livetiming/service/__init__.py
+++ src/livetiming/service/__init__.py
@@ -23,6 +23,7 @@ import argparse
 import copy
 import os
 import simplejson
+import time
 import txaio
 
 
@@ -411,6 +412,32 @@ class ReconnectingWebSocketClientFactory(WebSocketClientFactory, ReconnectingCli
     def clientConnectionLost(self, connector, reason):
         self.log.warn("Connection to upstream source lost! Reason: {reason}. Retrying...", reason=reason)
         self.retry(connector)
+
+
+class Watchdog(object):
+    def __init__(self, timeout, action_method, *action_args, **action_kwargs):
+        self.log = Logger()
+        self._timeout = timeout
+        self._action_method = action_method
+        self._action_args = action_args
+        self._action_kwargs = action_kwargs
+        self._call = LoopingCall(self._perform_check)
+
+    def start(self):
+        self._last_measure = time.time()
+        self._call.start(self._timeout / 2, False)
+
+    def stop(self):
+        self._call.stop()
+
+    def notify(self):
+        self._last_measure = time.time()
+
+    def _perform_check(self):
+        delta = time.time() - self._last_measure
+        if delta > self._timeout:
+            self.log.warn('WATCHDOG: {delta} since last notify received, triggering watchdog action', delta=delta)
+            self._action_method(*self._action_args, **self._action_kwargs)
 
 
 def parse_args():
