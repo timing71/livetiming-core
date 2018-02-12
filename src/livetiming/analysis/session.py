a2fd3ed9fe332e5ffd904075f6cee6b028953e4b
James Muscat <jamesremuscat@gmail.com>
2018-02-12 16:31:30 +0000

    Underscore-ize.

--- src/livetiming/analysis/session.py
+++ src/livetiming/analysis/session.py
@@ -1,7 +1,7 @@
 from livetiming.racing import FlagStatus
 
 
-def receiveStateUpdate(dc, old_state, new_state, colspec, timestamp):
+def receive_state_update(dc, old_state, new_state, colspec, timestamp):
     flag = FlagStatus.fromString(new_state["session"].get("flagState", "none"))
     old_flag = FlagStatus.fromString(old_state["session"].get("flagState", "none"))
     if flag != old_flag or not dc.session.this_period:
