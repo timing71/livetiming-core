829246dfa17daf1b6bb7132174b7df01555fb4df
James Muscat <jamesremuscat@gmail.com>
2018-02-14 10:48:37 +0000

    Dynamically import analysis modules.
    This hackily avoids a dependency cycle, plus it simplifies the
    configuration since we only need a list of strings rather than a dict.

--- src/livetiming/analysis/driver.py
+++ src/livetiming/analysis/driver.py
@@ -1,32 +1,5 @@
 from livetiming.racing import Stat
-
-
-class FieldExtractor(object):
-    def __init__(self, colSpec):
-        self.mapping = {}
-        for idx, col in enumerate(colSpec):
-            self.mapping[col] = idx
-
-    def get(self, car, field, default=None):
-        if car:
-            try:
-                return car[self.mapping[field]]
-            except KeyError:
-                return default
-        return default
-
-
-def per_car(func):
-    def inner(dc, old_state, new_state, colspec, timestamp):
-        f = FieldExtractor(colspec)
-        result = False
-        for idx, new_car in enumerate(new_state['cars']):
-            race_num = f.get(new_car, Stat.NUM)
-            if race_num:
-                old_car = next(iter([c for c in old_state["cars"] if f.get(c, Stat.NUM) == race_num] or []), None)
-                result = func(dc, race_num, old_car, new_car, f, timestamp) or result
-        return result
-    return inner
+from livetiming.analysis import FieldExtractor, per_car
 
 
 @per_car
