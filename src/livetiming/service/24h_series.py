223a6cbd3413cb0e0b31c1fa40fa7f2035eb745d
James Muscat <jamesremuscat@gmail.com>
2018-03-07 17:23:48 +0000

    Correctly pass extra `--tk` arg to superclass.

--- src/livetiming/service/24h_series.py
+++ src/livetiming/service/24h_series.py
@@ -2,5 +2,11 @@ from livetiming.service.timeservice_nl import Service as TSNLService
 
 
 class Service(TSNLService):
+    def __init__(self, args, extra_args):
+        if '--tk' not in extra_args:
+            extra_args.append('--tk')
+            extra_args.append('24hseries')
+        super(Service, self).__init__(args, extra_args)
+
     def getName(self):
         return "24H Series"
