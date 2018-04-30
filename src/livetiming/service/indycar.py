3c29a7247fd1dac0a6f513c5cc13a6ab628d6ffc
James Muscat <jamesremuscat@gmail.com>
2018-04-30 16:00:49 +0100

    Oval mode for IndyCar!
    This deletes the sector time columns, and adds turn 1/3 speed columns in
    their place.

--- src/livetiming/service/indycar.py
+++ src/livetiming/service/indycar.py
@@ -51,6 +51,15 @@ def parseSessionTime(formattedTime):
             return formattedTime
 
 
+def parseSpeed(formatted):
+    if formatted == '':
+        return 0
+    try:
+        return float(formatted)
+    except ValueError:
+        return 0
+
+
 def parseEventName(heartbeat):
     if "eventName" in heartbeat:
         event = "{} - ".format(heartbeat["eventName"])
@@ -102,6 +111,7 @@ class Service(lt_service):
         super(Service, self).__init__(args, extra_args)
         self.name = "IndyCar"
         self.description = "IndyCar"
+        self._oval_mode = False
 
     def getName(self):
         return self.name
@@ -118,18 +128,31 @@ class Service(lt_service):
             Stat.TYRE,
             Stat.PUSH_TO_PASS,
             Stat.GAP,
-            Stat.INT,
-            Stat.S1,
-            Stat.BS1,
-            Stat.S2,
-            Stat.BS2,
-            Stat.S3,
-            Stat.BS3,
+            Stat.INT
+        ] + self._get_sector_colspec() + [
             Stat.LAST_LAP,
             Stat.SPEED,
             Stat.BEST_LAP,
             Stat.PITS
         ]
+
+    def _get_sector_colspec(self):
+        if self._oval_mode:
+            return [
+                Stat.T1_SPEED,
+                Stat.BEST_T1_SPEED,
+                Stat.T3_SPEED,
+                Stat.BEST_T3_SPEED
+            ]
+        else:
+            return [
+                Stat.S1,
+                Stat.BS1,
+                Stat.S2,
+                Stat.BS2,
+                Stat.S3,
+                Stat.BS3
+            ]
 
     def getPollInterval(self):
         return 5
@@ -137,76 +160,16 @@ class Service(lt_service):
     def getRaceState(self):
         raw = self.getRawFeedData()
         cars = []
+
         timingResults = raw['timing_results']
-        seen = set()
-        filtered = [seen.add(car["no"]) or car for car in timingResults["Item"] if car["no"] not in seen]
-
-        fastSectors = [[9999, None], [9999, None], [9999, None]]
-        fastLap = [9999, None]
-
-        for car in sorted(filtered, key=lambda car: int(car["rank"])):
-            lastLapTime = parseTime(car["lastLapTime"])
-            bestLapTime = parseTime(car["bestLapTime"])
-
-            bs1 = parseTime(car.get('Best_I1', 0))
-            if bs1 > 0 and bs1 < fastSectors[0][0]:
-                fastSectors[0] = [bs1, car['no']]
-            bs2 = parseTime(car.get('Best_I2', 0))
-            if bs2 > 0 and bs2 < fastSectors[1][0]:
-                fastSectors[1] = [bs2, car['no']]
-            bs3 = parseTime(car.get('Best_I3', 0))
-            if bs3 > 0 and bs3 < fastSectors[2][0]:
-                fastSectors[2] = [bs3, car['no']]
-            if bestLapTime > 0 and bestLapTime < fastLap[0]:
-                fastLap = [bestLapTime, car['no']]
-
-            s1 = car.get('I1', '')
-            s2 = car.get('I2', '')
-            s3 = car.get('I3', '')
-
-            cars.append([
-                car["no"],
-                "PIT" if (car["status"] == "In Pit" or car["onTrack"] == "False") else "RUN",
-                "{0} {1}".format(car.get("firstName", ""), car.get("lastName", "")),
-                car["laps"],
-                map_tyre(car.get('Tire', ''))
-                [car["OverTake_Remain"], "ptp-active" if car["OverTake_Active"] == 1 else ""],
-                car["diff"] if "diff" in car else "",
-                car["gap"] if "gap" in car else "",
-                [s1, 'pb' if s1 == car.get('Best_I1', None) else ''],
-                [car.get('Best_I1', ''), 'old'],
-                [s2, 'pb' if s2 == car.get('Best_I2', None) else ''],
-                [car.get('Best_I2', ''), 'old'],
-                [s3, 'pb' if s3 == car.get('Best_I3', None) else ''],
-                [car.get('Best_I3', ''), 'old'],
-                [lastLapTime, "pb" if lastLapTime == bestLapTime and bestLapTime > 0 else ""],
-                car["LastSpeed"] if "LastSpeed" in car else "",
-                [bestLapTime, ""],
-                car["pitStops"]
-            ])
-
-        for car in cars:
-            num = car[0]
-            if num == fastLap[1]:
-                car[16] = [car[16][0], 'sb']
-                if car[16][0] == car[14][0]:
-                    car[14] = [car[14][0], 'sb-new' if car[12][0] != '' else 'sb']
-            if num == fastSectors[0][1]:
-                car[9] = [car[9][0], 'sb']
-                if car[8][0] == car[9][0]:
-                    car[8] = [car[8][0], 'sb']
-            if num == fastSectors[1][1]:
-                car[11] = [car[11][0], 'sb']
-                if car[10][0] == car[11][0]:
-                    car[10] = [car[10][0], 'sb']
-            if num == fastSectors[1][1]:
-                car[13] = [car[13][0], 'sb']
-                if car[12][0] == car[13][0]:
-                    car[12] = [car[12][0], 'sb']
 
         heartbeat = timingResults['heartbeat']
 
         shouldRepublish = False
+        trackType = heartbeat.get('trackType', None)
+        if trackType == "I" or trackType == "O":
+            self._oval_mode = True
+            shouldRepublish = True
         if "Series" in heartbeat and heartbeat["Series"] != self.name:
             self.name = heartbeat["Series"]
             shouldRepublish = True
@@ -225,6 +188,102 @@ class Service(lt_service):
         }
         if "totalLaps" in heartbeat:
             state["lapsRemain"] = int(heartbeat["totalLaps"]) - int(heartbeat["lapNumber"])
+
+        seen = set()
+        filtered = [seen.add(car["no"]) or car for car in timingResults["Item"] if car["no"] not in seen]
+
+        fastSectors = [[0, None], [0, None]] if self._oval_mode else [[9999, None], [9999, None], [9999, None]]
+        fastLap = [9999, None]
+
+        for car in sorted(filtered, key=lambda car: int(car["rank"])):
+            lastLapTime = parseTime(car["lastLapTime"])
+            bestLapTime = parseTime(car["bestLapTime"])
+
+            if bestLapTime > 0 and bestLapTime < fastLap[0]:
+                fastLap = [bestLapTime, car['no']]
+
+            if self._oval_mode:
+                t1 = parseSpeed(car.get('T1_SPD', 0))
+                bt1 = parseSpeed(car.get('Best_T1_SPD', 0))
+
+                t3 = parseSpeed(car.get('T3_SPD', ''))
+                bt3 = parseSpeed(car.get('Best_T3_SPD', ''))
+
+                if bt1 > 0 and bt1 > fastSectors[0][0]:
+                    fastSectors[0] = (bt1, car['no'])
+                if bt3 > 0 and bt3 > fastSectors[1][0]:
+                    fastSectors[1] = (bt3, car['no'])
+
+                sector_cols = [
+                    (t1 if t1 > 0 else '', 'pb' if t1 == bt1 else ''),
+                    (bt1 if bt1 > 0 else '', 'old'),
+                    (t3 if t3 > 0 else '', 'pb' if t3 == bt3 else ''),
+                    (bt3 if bt3 > 0 else '', 'old')
+                ]
+            else:
+
+                bs1 = parseTime(car.get('Best_I1', 0))
+                if bs1 > 0 and bs1 < fastSectors[0][0]:
+                    fastSectors[0] = [bs1, car['no']]
+                bs2 = parseTime(car.get('Best_I2', 0))
+                if bs2 > 0 and bs2 < fastSectors[1][0]:
+                    fastSectors[1] = [bs2, car['no']]
+                bs3 = parseTime(car.get('Best_I3', 0))
+                if bs3 > 0 and bs3 < fastSectors[2][0]:
+                    fastSectors[2] = [bs3, car['no']]
+
+                s1 = car.get('I1', '')
+                s2 = car.get('I2', '')
+                s3 = car.get('I3', '')
+
+                sector_cols = [
+                    [s1, 'pb' if s1 == car.get('Best_I1', None) else ''],
+                    [car.get('Best_I1', ''), 'old'],
+                    [s2, 'pb' if s2 == car.get('Best_I2', None) else ''],
+                    [car.get('Best_I2', ''), 'old'],
+                    [s3, 'pb' if s3 == car.get('Best_I3', None) else ''],
+                    [car.get('Best_I3', ''), 'old']
+                ]
+
+            cars.append([
+                car["no"],
+                "PIT" if (car["status"] == "In Pit" or car["onTrack"] == "False") else "RUN",
+                "{0} {1}".format(car.get("firstName", ""), car.get("lastName", "")),
+                car["laps"],
+                map_tyre(car.get('Tire', '')),
+                [car["OverTake_Remain"], "ptp-active" if car["OverTake_Active"] == 1 else ""],
+                car["diff"] if "diff" in car else "",
+                car["gap"] if "gap" in car else ""
+            ] + sector_cols + [
+                [lastLapTime, "pb" if lastLapTime == bestLapTime and bestLapTime > 0 else ""],
+                car["LastSpeed"] if "LastSpeed" in car else "",
+                [bestLapTime, ""],
+                car["pitStops"]
+            ])
+
+        bestLapIdx = 14 if self._oval_mode else 16
+        lastLapIdx = 12 if self._oval_mode else 14
+        lastSectorIdx = 10 if self._oval_mode else 12
+
+        for car in cars:
+            num = car[0]
+            if num == fastLap[1]:
+                car[bestLapIdx] = [car[bestLapIdx][0], 'sb']
+                if car[bestLapIdx][0] == car[lastLapIdx][0]:
+                    car[lastLapIdx] = [car[lastLapIdx][0], 'sb-new' if car[lastSectorIdx][0] != '' else 'sb']
+            if num == fastSectors[0][1]:
+                car[9] = [car[9][0], 'sb']
+                if car[8][0] == car[9][0]:
+                    car[8] = [car[8][0], 'sb']
+            if num == fastSectors[1][1]:
+                car[11] = [car[11][0], 'sb']
+                if car[10][0] == car[11][0]:
+                    car[10] = [car[10][0], 'sb']
+            if not self._oval_mode and num == fastSectors[1][1]:
+                car[13] = [car[13][0], 'sb']
+                if car[12][0] == car[13][0]:
+                    car[12] = [car[12][0], 'sb']
+
         return {"cars": cars, "session": state}
 
     def getRawFeedData(self):
