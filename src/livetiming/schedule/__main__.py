3497d441f8dbbcd71ab8dc140e062777619fe910
James Muscat <jamesremuscat@gmail.com>
2018-05-02 18:17:03 +0100

    Create a `livetiming-schedule` entry point.

--- src/livetiming/schedule/__main__.py
+++ src/livetiming/schedule/__main__.py
@@ -5,28 +5,32 @@ import argparse
 import importlib
 import os
 
-load_env()
-
-service = get_gcal_service()
-
 COMMANDS = [
     'assist',
     'check',
     'list'
 ]
 
-parser = argparse.ArgumentParser()
 
-subparsers = parser.add_subparsers()
+def main():
+    load_env()
 
-for cmd in COMMANDS:
-    mod = importlib.import_module('livetiming.schedule.{}'.format(cmd))
-    if hasattr(mod, 'run') and callable(getattr(mod, 'run')):
-        mod_parser = subparsers.add_parser(cmd)
-        mod_parser.set_defaults(func=getattr(mod, 'run'))
-        if hasattr(mod, 'add_parser_args') and callable(getattr(mod, 'add_parser_args')):
-            getattr(mod, 'add_parser_args')(mod_parser)
+    service = get_gcal_service()
+    parser = argparse.ArgumentParser()
 
-args = parser.parse_args()
+    subparsers = parser.add_subparsers()
 
-args.func(service, args)
+    for cmd in COMMANDS:
+        mod = importlib.import_module('livetiming.schedule.{}'.format(cmd))
+        if hasattr(mod, 'run') and callable(getattr(mod, 'run')):
+            mod_parser = subparsers.add_parser(cmd)
+            mod_parser.set_defaults(func=getattr(mod, 'run'))
+            if hasattr(mod, 'add_parser_args') and callable(getattr(mod, 'add_parser_args')):
+                getattr(mod, 'add_parser_args')(mod_parser)
+
+    args = parser.parse_args()
+
+    args.func(service, args)
+
+if __name__ == "__main__":
+    main()
