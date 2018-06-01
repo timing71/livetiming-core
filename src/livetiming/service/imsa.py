e306257b07756641530322f841d7aeb8bd20e0cb
James Muscat <jamesremuscat@gmail.com>
2018-06-01 13:03:26 +0100

    Migrate IMSA timing to Al Kamel v2.

--- src/livetiming/service/imsa.py
+++ src/livetiming/service/imsa.py
@@ -1,3 +1,6 @@
-from imsa_alkamel import Service
+from alkamel2 import Service as AlKamelService
 
-__all__ = [Service]
+
+class Service(AlKamelService):
+    def __init__(self, args, extra_args):
+        super(Service, self).__init__(args, extra_args, feed="imsa")
