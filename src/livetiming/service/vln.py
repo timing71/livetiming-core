1829a74c87b205859b8954ed0a9b4029049b568d
James Muscat <jamesremuscat@gmail.com>
2018-09-06 13:36:11 +0100

    Shortcutty service for VLN.

---
+++ src/livetiming/service/vln.py
@@ -0,0 +1,10 @@
+from livetiming.service.wige import Service as wige
+
+
+class Service(wige):
+    def __init__(self, args, extra_args):
+        extra_args.append('--ws')
+        extra_args.append('wss://livetiming.azurewebsites.net/event-20/ws')
+        extra_args.append('-e')
+        extra_args.append('20')
+        super(Service, self).__init__(args, extra_args)
