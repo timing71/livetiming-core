a088c7ce963c55a776353b748fbe0cfa70a09a2c
James Muscat <james.muscat@alces-software.com>
2018-09-25 16:08:35 +0100

    Bunch of improvements, including correct car ordering.

--- src/livetiming/chrono/__main__.py
+++ src/livetiming/chrono/__main__.py
@@ -1,6 +1,7 @@
 import argparse
 
 from livetiming.chrono import alkamel
+from livetiming.racing import Stat
 from livetiming.recording import TimingRecorder
 import uuid
 
@@ -23,6 +24,7 @@ def _parse_args():
         subparser.set_defaults(message_generators=format_module.message_generators)
         subparser.set_defaults(sort_cars=format_module.sort_cars)
         subparser.set_defaults(get_start_time=format_module.get_start_time)
+        subparser.set_defaults(colspec=format_module.COLSPEC)
 
     parser.add_argument('--output', '-o', help='Output filename', default='output.zip')
 
@@ -31,9 +33,9 @@ def _parse_args():
 
 def main():
     args = _parse_args()
-    initial_state = args.create_initial_state(args)
+    initial_state = args.create_initial_state(args, [None, None, None, None, 0])
 
-    events = sorted(args.create_events(args), key=lambda e: e[0])
+    events = sorted(args.create_events(args), key=lambda e: e.timestamp)
 
     message_generators = args.message_generators()
 
@@ -48,18 +50,26 @@ def main():
     recorder.writeManifest({
         'description': 'Converted chrono dump',
         'name': 'converted',
-        'uuid': uuid.uuid4().hex
+        'uuid': uuid.uuid4().hex,
+        'colSpec': map(lambda s: s.value if isinstance(s, Stat) else s, args.colspec)
     })
     next_frame_threshold = 0
 
     session_start_time = args.get_start_time(args)
     recorder.writeState(state, session_start_time)
 
-    for evt_time, evt in events:
+    for evt in events:
+        evt_time = evt.timestamp
         car_state = evt(car_state)
+
+        elapsed = evt_time - session_start_time
+
         new_state = {
             'cars': args.sort_cars(args, car_state.values()),
-            'session': {'timeElapsed': evt_time - session_start_time},
+            'session': {
+                'timeElapsed': elapsed,
+                'timeRemain': int(args.duration) - elapsed
+            },
             'messages': state['messages']
         }
 
@@ -71,7 +81,7 @@ def main():
 
         state = new_state
 
-    recorder.writeState(state, events[-1][0])
+    recorder.writeState(state, events[-1].timestamp)
 
 
 def _generate_messages(generators, timestamp, old_state, new_state):
