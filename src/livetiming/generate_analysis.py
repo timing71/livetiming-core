619804a94f1c14cf02e989e30ffc32e1aedae032
James Muscat <james.muscat@alces-software.com>
2019-01-08 17:22:08 +0000

    Don't accidentally write output to temp dir then delete it!

--- src/livetiming/generate_analysis.py
+++ src/livetiming/generate_analysis.py
@@ -1,12 +1,13 @@
 from livetiming.recording import generate_analysis
 
+import os
 import sys
 
 
 def main():
     # Generate an analysis data dump from a recording file
     recFile = sys.argv[1]
-    generate_analysis(recFile, 'data.out.json', True)
+    generate_analysis(recFile, os.path.join(os.getcwd(), 'data.out.json'), True)
 
 
 if __name__ == '__main__':
