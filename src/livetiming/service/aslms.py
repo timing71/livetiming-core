2af78d4dcf8249e840a384fd721ed611625ceb8b
James Muscat <james.muscat@alces-software.com>
2019-01-11 18:10:08 +0000

    Shortcut service for AsLMS.

---
+++ src/livetiming/service/aslms.py
@@ -0,0 +1,12 @@
+from livetiming.service.timeservice_nl import Service as TSNLService
+
+
+class Service(TSNLService):
+    def __init__(self, args, extra_args):
+        if '--tk' not in extra_args:
+            extra_args.append('--tk')
+            extra_args.append('asianlemansseries')
+        super(Service, self).__init__(args, extra_args)
+
+    def getName(self):
+        return "Asian Le Mans Series"
