80d0aab6fc7d63cbcd8c31fdafe64f26ae0c2d35
James Muscat <jamesremuscat@gmail.com>
2019-04-10 11:21:41 +0100

    Make Blancpain service year-agnostic.

--- src/livetiming/service/blancpain.py
+++ src/livetiming/service/blancpain.py
@@ -1,7 +1,9 @@
 from livetiming.service.swisstiming import Service as SwissTimingService
 
+import time
+
 
 class Service(SwissTimingService):
     attribution = ['Blancpain / SRO', 'https://www.blancpain-gt-series.com/']
-    URL_BASE = "http://livecache.sportresult.com/node/db/RAC_PROD/SRO_2018_"
+    URL_BASE = "http://livecache.sportresult.com/node/db/RAC_PROD/SRO_{year}_".format(year=time.strftime("%Y"))
     default_name = 'Blancpain GT Series'
