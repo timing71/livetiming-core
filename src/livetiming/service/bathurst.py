9a49d1103bae3ec33843aef54339fd1bc402b317
James Muscat <jamesremuscat@gmail.com>
2019-04-15 13:03:20 +0100

    More robust way of finding TSNL's Bathurst service.

--- src/livetiming/service/bathurst.py
+++ src/livetiming/service/bathurst.py
@@ -2,8 +2,11 @@ from livetiming.service.timeservice_nl import Service as TSNLService
 
 
 class Service(TSNLService):
+    def __init__(self, args, extra_args):
+        if '--tk' not in extra_args:
+            extra_args.append('--tk')
+            extra_args.append('bathurst')
+        super(Service, self).__init__(args, extra_args)
+
     def getName(self):
         return "Bathurst"
-
-    def getTrackID(self):
-        return "59225c5480a74b178deaf992976595c3"
