482d08ddf765dbf86b3bc3eb9f4ada44a4c946da
James Muscat <jamesremuscat@gmail.com>
2019-04-29 18:20:51 +0100

    Dynamically generate as many sector columns as are required.
    FSR the IMSA data has about a million different timing sectors...

--- src/livetiming/service/__tests__/test_hhtiming.py
+++ src/livetiming/service/__tests__/test_hhtiming.py
@@ -6,9 +6,11 @@ import os
 import pytest
 
 
+STATE_DUMP_FILE = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'data', 'hhtiming.json')
+
 class ServiceForTest(Service):
     def _state_dump_file(self):
-        return os.path.join(os.path.dirname(os.path.realpath(__file__)), 'data', 'hhtiming.json')
+        return STATE_DUMP_FILE
 
 
 @pytest.fixture
@@ -60,3 +62,32 @@ def test_calculate_gap(service, colspec, state):
     last_car = state['cars'][-1]
     assert last_car[gap_idx] == '9 laps'
     assert last_car[int_idx] == '3 laps'
+
+
+def test_compute_colspec(service, colspec):
+    pre_sector_stats = [
+        Stat.NUM,
+        Stat.STATE,
+        Stat.CLASS,
+        Stat.TEAM,
+        Stat.DRIVER,
+        Stat.CAR,
+        Stat.LAPS,
+        Stat.GAP,
+        Stat.INT
+    ]
+
+    assert colspec[:len(pre_sector_stats)] == pre_sector_stats
+
+    post_sector_stats = [
+        Stat.LAST_LAP,
+        Stat.BEST_LAP,
+        Stat.PITS
+    ]
+
+    assert colspec[-len(post_sector_stats):] == post_sector_stats
+
+    non_sector_stats = len(pre_sector_stats) + len(post_sector_stats)
+
+    expected_num_sectors = len(service.protocol.track['OrderedListOfOnTrackSectors']['$values'])
+    assert len(colspec) == non_sector_stats + (expected_num_sectors * 2)
