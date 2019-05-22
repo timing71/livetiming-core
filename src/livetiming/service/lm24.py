ac7cc2e3965806051e6a9b221212aaa04c1074ea
James Muscat <jamesremuscat@gmail.com>
2019-05-22 14:34:44 +0100

    lm24 service should point to actual WEC service.

--- src/livetiming/service/lm24.py
+++ src/livetiming/service/lm24.py
@@ -1,12 +1,9 @@
-from livetiming.service.wec_unified import Service as WECService
+from livetiming.service.wec import Service as WECService
 
 
 class Service(WECService):
     attribution = ['WEC / ACO', 'http://www.fiawec.com/']
     initial_description = ''
-
-    def __init__(self, args, extra_args):
-        super(Service, self).__init__(args, extra_args)
 
     def getName(self):
         return "Le Mans 24 Hours"
