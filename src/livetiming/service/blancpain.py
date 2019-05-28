16f513426909e3f5556d3c68d4b657b51d1763a8
James Muscat <jamesremuscat@gmail.com>
2019-05-28 12:55:22 +0100

    Add namespace/profile for Blancpain.

--- src/livetiming/service/blancpain.py
+++ src/livetiming/service/blancpain.py
@@ -5,5 +5,6 @@ import time
 
 class Service(SwissTimingService):
     attribution = ['Blancpain / SRO', 'https://www.blancpain-gt-series.com/']
-    URL_BASE = "http://livecache.sportresult.com/node/db/RAC_PROD/SRO_{year}_".format(year=time.strftime("%Y"))
+    namespace = 'RAC_PROD'
+    profile = 'SRO'
     default_name = 'Blancpain GT Series'
