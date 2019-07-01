e1215fcb9f60ee53eded318875e8821ffa8eff19
James Muscat <jamesremuscat@gmail.com>
2019-07-01 14:51:23 +0100

    Delete obsolete services.

--- src/livetiming/service/monza.py
+++
@@ -1,6 +0,0 @@
-from livetiming.service.alkamel import Service as alkamel_service
-
-
-class Service(alkamel_service):
-    def __init__(self, args, extra_args):
-        super(Service, self).__init__(args, extra_args, feed="monza")
