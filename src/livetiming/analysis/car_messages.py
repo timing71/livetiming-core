bea5e79e823df3dfe51a4b300f881d86bd659fc3
James Muscat <jamesremuscat@gmail.com>
2019-07-15 13:57:45 +0100

    Really fix per-car messages.

--- src/livetiming/analysis/car_messages.py
+++ src/livetiming/analysis/car_messages.py
@@ -1,13 +1,19 @@
 from livetiming.racing import Stat
-from livetiming.analysis import per_car
 
 
-@per_car('car_messages', get_data)
 def receive_state_update(dc, race_num, position, old_car, new_car, f, flag, timestamp, new_messages):
     car = dc.car(race_num)
     relevant_messages = [m for m in new_messages if _message_is_relevant(race_num, m)]
     car.messages += relevant_messages
-    return len(relevant_messages) > 0
+
+    if len(relevant_messages) > 0:
+        return (
+            'car_messages/{}'.format(race_num),
+            {
+                race_num: [car.messages]
+            }
+        )
+    return []
 
 
 def _message_is_relevant(race_num, message):
