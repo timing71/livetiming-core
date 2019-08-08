0edcbe8d777a13272b98c85279f0d11d08017dfe
James Muscat <jamesremuscat@gmail.com>
2019-08-08 10:22:17 +0100

    Fix last-sector detection for TSNL.

--- src/livetiming/service/timeservice_nl.py
+++ src/livetiming/service/timeservice_nl.py
@@ -449,7 +449,7 @@ class Service(DuePublisher, lt_service):
                 s3Idx = colSpec.index(Stat.S3) if Stat.S3 in colSpec else None
 
                 if len(result[lastIdx]) == 2 and result[lastIdx][1] == "sb" and result[lastIdx][0] == result[bestIdx][0]:
-                    if result[stateIdx] == "RUN" and (s3Idx is None or (result[s3Idx] != '' and result[s3Idx][0] > 0)):  # Not if in pits or have completed next lap S1
+                    if result[stateIdx] == "RUN" and (s3Idx is None or (result[s3Idx][0] != '' and result[s3Idx][0] > 0)):  # Not if in pits or have completed next lap S1
                         result[lastIdx] = (result[lastIdx][0], "sb-new")
 
         return result
