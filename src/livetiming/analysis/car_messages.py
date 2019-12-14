728efc1b2a1c0a193746de395e7176f7f66ed281
James Muscat <jamesremuscat@gmail.com>
2019-12-14 15:38:11 +0000

    Handle mismatched types for race number.

--- src/livetiming/analysis/car_messages.py
+++ src/livetiming/analysis/car_messages.py
@@ -17,9 +17,11 @@ def receive_state_update(dc, race_num, position, old_car, new_car, f, flag, time
 
 
 def _message_is_relevant(race_num, message):
-    return len(message) > 4 and \
-        message[4] == race_num and \
-        message[3] != 'pit' and message[3] != 'out'
+
+    race_num_match = len(message) > 4 and \
+        str(message[4]) == str(race_num)
+
+    return race_num_match and message[3] != 'pit' and message[3] != 'out'
 
 
 def get_data(dc):
