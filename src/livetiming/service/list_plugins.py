ec4f691c49fb0321a210a6c2869064ef7e14bc1a
James Muscat <jamesremuscat@gmail.com>
2020-10-13 20:40:26 +0100

    Add a human-readable output option to `livetiming-plugins`.

--- src/livetiming/service/list_plugins.py
+++ src/livetiming/service/list_plugins.py
@@ -1,9 +1,25 @@
 from livetiming.service import get_plugin_source
 
+import argparse
 import simplejson
 
 
+BOLD = '\033[1m'
+END = '\033[0m'
+
+
 def main():
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '-H',
+        '--human-readable',
+        action='store_true',
+        help='Give human-readable output (default is to output JSON)'
+    )
+
+    args = parser.parse_args()
+
     with get_plugin_source() as source:
         plugins = list(
             filter(
@@ -16,7 +32,21 @@ def main():
             p: getattr(source.load_plugin(p), '__spec', {}) for p in plugins
         }
 
-        print(simplejson.dumps(plugin_stats))
+        if args.human_readable:
+            if plugin_stats:
+                print('Available plugins:')
+                for name, plugin in plugin_stats.items():
+                    print('{}{}{}'.format(
+                        BOLD,
+                        name,
+                        END
+                    ))
+                    if 'description' in plugin:
+                        print('\t{}'.format(plugin['description']))
+            else:
+                print('No plugins available')
+        else:
+            print(simplejson.dumps(plugin_stats))
 
 
 if __name__ == '__main__':
